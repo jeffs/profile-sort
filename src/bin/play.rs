800ebe4cdd154aa82cd3648e255edec0428d595b
Jeff Schwab <jeff@unbuggy.com>
2022-07-22 13:20:39 -0400

    Use ? on Option; de/serialize test count

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,28 +1,38 @@
-use itertools::Itertools as _; // for group_by
-
-/// Returns the values 1 through 9, inclusive.
-fn digits() -> std::ops::RangeInclusive<u32> {
-    1..=9
+pub fn sqrt(x: f64) -> Option<f64> {
+    (!x.is_sign_negative()).then(|| x.sqrt())
 }
 
-/// Returns all 81 numbers having (in base 10) two non-zero digits.
-fn numbers() -> impl Iterator<Item = u32> {
-    digits().flat_map(|d0| digits().map(move |d1| d0 * 10 + d1))
+pub fn sum_sqrts(x: f64, y: f64) -> Option<f64> {
+    Some(sqrt(x)? + sqrt(y)?)
 }
 
-fn main() {
-    numbers()
-        .flat_map(|a| numbers().filter(move |&b| b <= a).map(move |b| (a, b)))
-        .filter(|(a, b)| a * b % 100 == (a % 10) * (b % 10))
-        .inspect(|(a, b)| {
-            println!("{a:2} * {b:2} == {:4}", a * b);
-        })
-        .group_by(|&(a, _)| a)
-        .into_iter()
-        .map(|(key, group)| (key, group.count()))
-        .for_each(|(key, count)| {
-            println!("---------------");
-            println!("             {count:2}");
-            println!("");
-        });
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_io() {
+        let n = std::fs::read("count")
+            .ok()
+            .and_then(|bytes| bytes.try_into().ok().map(i32::from_be_bytes))
+            .unwrap_or(1);
+        println!("{n}");
+        std::fs::write("count", (n + 1).to_be_bytes()).expect("can't write count");
+    }
+
+    #[test]
+    fn test_sqrt() {
+        assert_eq!(Some(2.0), sqrt(4.0));
+        assert_eq!(None, sqrt(-4.0));
+    }
+
+    #[test]
+    fn test_sum_sqrts() {
+        assert_eq!(Some(5.0), sum_sqrts(4.0, 9.0));
+        assert_eq!(None, sum_sqrts(4.0, -9.0));
+        assert_eq!(None, sum_sqrts(-4.0, 9.0));
+        assert_eq!(None, sum_sqrts(-4.0, -9.0));
+    }
 }
+
+fn main() {}
